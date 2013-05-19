96dd94125d5a8b75d5e3edeb5c18daa1590462a4
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-19 22:58:11 +0200

    Make it a python package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datastringer',
+    version='0.1.0',
+    description="Client for datawi.re, submits data to the data wire service.",
+    long_description="",
+    classifiers=[],
+    keywords='data record notifications service client wire',
+    author='Friedrich Lindenberg',
+    author_email='info@datawi.re',
+    url='http://github.com/pudo/datastringer',
+    license='AGPLv3',
+    py_modules=['datastringer'],
+    zip_safe=False,
+    install_requires=[
+        "requests>=1.2"
+    ],
+    tests_require=[],
+    entry_points=""" """,
+)
